3e27652bdff8a6ac4f5e82ef286582071bbb5d6f
michalliu <michalliu@foxmail.com>
2013-09-03 19:34:11 +0800

    API调用完成后前后移除所有事件。API调用超时时间设为1小时（永不超时）

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,22 @@
-/*global require,exports,console*/
+/*global require,exports,console,SERVER_EVENT*/
 /*
  * API Routes
  */
 
 var apiModulesDir="./api_handlers/";
 
+function removeListeners(){
+	SERVER_EVENT.removeAllListeners("resouceOwnerDisconnected");
+	SERVER_EVENT.removeAllListeners("resouceOwnerConnected");
+	SERVER_EVENT.removeAllListeners("resouceOwnerPaStart");
+	SERVER_EVENT.removeAllListeners("resouceOwnerPaStop");
+}
+
 exports.index = function(req, res){
 	var apiName = req.params[0];
 	var apiHandler, apiModule;
+	// API Request has a very long timeout
+	req.connection.setTimeout(60 * 60 * 1000);
 	try {
 		apiModule=require(apiModulesDir+apiName);
 	} catch(ex){}
@@ -16,12 +25,14 @@ exports.index = function(req, res){
 		apiHandler=apiModule.handler;
 		apiHandler.removeAllListeners("exit");
 		apiHandler.on("exit", function (exitcode, data) {
+			removeListeners();
 			res.json(200, {
 				ret:exitcode,
 				timestamp:Date.now(),
 				data: data
 			});
 		});
+		removeListeners();
 		apiHandler.tick();
 	} else {
 		res.json(200, {
